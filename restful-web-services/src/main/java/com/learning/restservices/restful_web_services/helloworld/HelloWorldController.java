8684cf86314f8ddaf39cd7883f1de56a23af07bf
abhisheksingh <abhisheksingh.geek123@gmail.com>
2024-07-17 08:39:12 +0530

    REST API using @GetMapping

--- restful-web-services/src/main/java/com/learning/restservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/learning/restservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.learning.restservices.restful_web_services.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
-	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello-World";
 	}
