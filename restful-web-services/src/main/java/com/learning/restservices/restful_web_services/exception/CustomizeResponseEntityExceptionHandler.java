091dd00b685d75e33d87126ea0116b2a5e32d3bc
abhisheksingh <abhisheksingh.geek123@gmail.com>
2024-07-22 01:11:31 +0530

    Implementing validation for RESt API

--- restful-web-services/src/main/java/com/learning/restservices/restful_web_services/exception/CustomizeResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/learning/restservices/restful_web_services/exception/CustomizeResponseEntityExceptionHandler.java
@@ -2,8 +2,12 @@ package com.learning.restservices.restful_web_services.exception;
 
 import java.time.LocalDate;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -25,5 +29,13 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
 		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
 		
 	    return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
 	}
 }
