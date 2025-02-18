f09b76d500aa1ccf7e5af2a5010bbecc3e2f2f52
abhisheksingh <abhisheksingh.geek123@gmail.com>
2025-02-18 09:52:29 +0530

    Versioning REST API - Using header

--- restful-web-services/src/main/java/com/learning/restservices/restful_web_services/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/learning/restservices/restful_web_services/versioning/VersioningPersonController.java
@@ -1,7 +1,6 @@
 package com.learning.restservices.restful_web_services.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,12 +17,22 @@ public class VersioningPersonController {
 	}
 	
 	@GetMapping(path="/person", params = "version=1")
-	public PersonV1 getFirstVersionOfPersonRequstParameter() {
+	public PersonV1 getFirstVersionOfPersonRequestParameter() {
 		return new PersonV1("Bob Charlie using param");
 	}
 
 	@GetMapping(path="/person", params = "version=2")
 	public PersonV2 getSecondVersionOfPersonRequestParameter() {
+		return new PersonV2(new Name("Bob V2", "Charlie V2"));
+	}
+	
+	@GetMapping(path="/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("Bob Charlie using header");
+	}
+	
+	@GetMapping(path="/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
 		return new PersonV2(new Name("Bob V2", "Charlie V2"));
 	}
 }
