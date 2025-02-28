21cb6d8321fe8cc864b9802b9113ed87dcbbe59c
abhisheksingh <abhisheksingh.geek123@gmail.com>
2025-02-28 21:10:56 +0530

    Post Entity

---
+++ restful-web-services/src/main/java/com/learning/restservices/restful_web_services/user/Post.java
@@ -0,0 +1,15 @@
+package com.learning.restservices.restful_web_services.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Post {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String description;
+
+}
